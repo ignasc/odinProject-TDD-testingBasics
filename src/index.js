8fcd8987bd752121ef28c27f648f1af29ecf184c
Ignas <15948796+ignasc@users.noreply.github.com>
2025-12-09 15:47:23 +0000

    Clean up: delete unused files and modify index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import "./resetcss.css";
-import "./styles.css";
-
-import showTestMessage from "./testModule";
-
-showTestMessage();
+console.log(
+    '\n-------------------------------------\nThis is a file that contains a few modules, which were built using test driven development practices.\n\nPlease run \"npm run test\" from root directory to run the tests on all implemented functions.\n-------------------------------------'
+);
